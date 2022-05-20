cc2a081f28e9efc9c4c00b3e74f184b0616f11db
TrizBooB <matthieu.fournet@gmail.com>
2022-05-20 13:45:04 +0000

    js_operator

--- JS cours/script.js
+++ JS cours/script.js
@@ -1,21 +1,12 @@
-var nom = "Matt";
-var nom_len = nom.length;
-var x = 12.10;
-var x_str = x.toString();
-var x_string = '12'
-var x_int = parseInt(x_string);
-var x_float = parseFloat(x_str)
+var x = 12;
+var y;
+y = 5;
+var result = ++x;
 
-console.log(x_str);
-console.log(x_int);
-console.log(x_float);
+console.log(result);
 
 
-var myString = "Hello Matt";
-var position = myString.indexOf("Matt");
-var myNewString = myString.replace("Matt", "John");
-var string2 = myString + ", " + myNewString;
+var num = 1;
+num += 2;
 
-console.log(position);
-console.log(myNewString);
-console.log(string2);
+console.log(num);
